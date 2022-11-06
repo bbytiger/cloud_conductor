24759151074153dd5733fb7365b46a7d7faaf0ca
bbytiger <andy200212@gmail.com>
2022-11-06 15:53:11 -0500

    got data transfer in aws

--- aws.py
+++ aws.py
@@ -1,3 +1,4 @@
+import uuid
 import boto3
 
 # for pricing, reference: https://aws.amazon.com/s3/pricing/?p=pm&c=s3&z=4
@@ -11,26 +12,48 @@ def create_bucket(name, region, bucket_config):
     print(bucket_config)
     s3_client.create_bucket(
         Bucket=name, CreateBucketConfiguration=bucket_config)
+
+def upload_data(key, bucket):
+    f = open('data/test.txt', 'rb') 
+    bucket.upload_fileobj(f, key)
+
+def transfer_data(src_bucket_name, src_key, dst_bucket, dst_key):
+    dst_bucket.copy(
+        {
+            'Bucket': src_bucket_name, 
+            'Key': src_key
+        },
+        dst_key
+    )
     
 if __name__ == '__main__':
-    src_bucket = 'cs243-src-bucket'
-    dst_bucket = 'cs243-dst-bucket'
+    # initial constants
+    src_bucket_name = 'cs243-src-bucket-{}'.format(uuid.uuid4())
+    dst_bucket_name = 'cs243-dst-bucket-{}'.format(uuid.uuid4())
+    src_key = 'cs243-src-key-{}'.format(uuid.uuid4())
+    dst_key = 'cs243-dst-key-{}'.format(uuid.uuid4())
     source_region = 'us-west-2'
     destination_region = 'eu-west-1'
 
     # determined by the destination region
     perGBtransfercost = 0.02
 
-    s3 = boto3.client('s3')
-    response = s3.list_buckets()
+    # list buckets
+    s3 = boto3.resource('s3')
+    s3_client = boto3.client('s3')
+    response = s3_client.list_buckets()
     print(response)
 
     # create bucket in source region
-    create_bucket(name=src_bucket, region=source_region, bucket_config={})
+    create_bucket(name=src_bucket_name, region=source_region, bucket_config={})
+    src_bucket = s3.Bucket(src_bucket_name)
 
     # upload to source bucket (ingress)
+    upload_data(src_key, src_bucket)
 
     # create bucket in destination region
-    create_bucket(name=dst_bucket, region=destination_region, bucket_config={})
-
+    create_bucket(name=dst_bucket_name, region=destination_region, bucket_config={})
+    dst_bucket = s3.Bucket(dst_bucket_name)
+    
     # transfer to destination bucket (egress)
+    transfer_data(src_bucket_name, src_key, dst_bucket, dst_key)
