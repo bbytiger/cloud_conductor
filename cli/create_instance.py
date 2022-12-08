c4ec013e6f57de4cc2fbdec40dac98a3afa48434
bbytiger <andy200212@gmail.com>
2022-12-08 14:50:57 -0500

    working on credentials

---
+++ cli/create_instance.py
@@ -0,0 +1,10 @@
+import boto3
+
+def launch_instance_aws():
+    pass
+
+def launch_instance_gcp():
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ cli/credentials.py
@@ -0,0 +1,9 @@
+import subprocess
+# configurations only for linux currently setup
+
+def setup_aws():
+    pass
+
+
+if __name__ == "__main__":
+    setup_aws()
