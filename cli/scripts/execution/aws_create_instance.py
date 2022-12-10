081242e03be667df8b14cacbfb48b08f5badac11
bbytiger <andy200212@gmail.com>
2022-12-10 12:47:34 -0500

    refactor into functions

--- cli/scripts/execution/aws_create_instance.py
+++ cli/scripts/execution/aws_create_instance.py
@@ -6,32 +6,42 @@ keyname = 'tmp_cloud_conduct_{}'.format(uuid.uuid4())
 imageid = 'ami-0574da719dca65348' # default set to ubuntu
 instancetype = 't2.micro'
 
-ec2 = boto3.client('ec2', region_name=regionname)
-print(ec2.describe_key_pairs())
+def get_ec2_client(regionname):
+    ec2 = boto3.client('ec2', region_name=regionname)
+    print(ec2.describe_key_pairs())
+    return ec2
 
-response = ec2.create_key_pair(KeyName=keyname)
-print(response)
+def create_RSA_key_pair(ec2, keyname):
+    response = ec2.create_key_pair(KeyName=keyname)
+    print(response)
+    return response
 
-ec2_config = {
-    'ImageId': imageid,
-    'InstanceType': instancetype,
-    'KeyName': keyname,
-    'MinCount': 1,
-    'MaxCount': 1
-}
+def delete_RSA_key_pair(ec2, keyname):
+    response = ec2.delete_key_pair(KeyName=keyname)
+    print(response)
+    return response
 
-response = ec2.run_instances(**ec2_config)
-print(response)
+def create_instance(ec2, imageid, instancetype, keyname):
+    ec2_config = {
+        'ImageId': imageid,
+        'InstanceType': instancetype,
+        'KeyName': keyname,
+        'MinCount': 1,
+        'MaxCount': 1
+    }
+    response = ec2.run_instances(**ec2_config)
+    print(response)
+    instanceId = response["Instances"][0]["InstanceId"]
+    return instanceId
 
-instanceId = response["Instances"][0]["InstanceId"]
+def terminate_instance(ec2, instanceId):
+    response = ec2.terminate_instances(
+        InstanceIds=[instanceId]
+    )
+    print(response)
+    return response
 
-response = ec2.terminate_instances(
-    InstanceIds=[instanceId]
-)
-print(response)
 
-response = ec2.delete_key_pair(KeyName=keyname)
-print(response)
 
 
 
